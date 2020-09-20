8bd99b843673f59af2efa6d6192b78eace40552f
西决 <1135586141@qq.com>
2020-09-20 16:22:48 +0800

    team1 完成 今日任务 addCart的完成

--- gmall-common/src/main/java/com/atguigu/gmall/common/bean/TestDev1.java
+++ gmall-common/src/main/java/com/atguigu/gmall/common/bean/TestDev1.java
@@ -5,4 +5,11 @@ package com.atguigu.gmall.common.bean;
  * @create 2020-09-20 16:16
  */
 public class TestDev1 {
+    /**
+     * dev1 今天的任务完成 提交
+     * 不需要合并
+     */
+    public void addCart(){
+
+    }
 }
